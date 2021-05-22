b6bafb0d0007169aea60209e95ad334030082877
oooo3994 <oooo3994@nate.com>
2021-05-22 18:48:08 +0900

    firstCommit

---
+++ main.py
@@ -0,0 +1,180 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from konlpy.tag import Okt
+from tensorflow.keras.preprocessing.text import Tokenizer
+from collections import Counter
+
+from sklearn.model_selection import train_test_split
+from tensorflow.python.keras.preprocessing.sequence import pad_sequences
+
+from tensorflow.keras.layers import Embedding, Dense, GRU
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import load_model
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+
+okt = Okt()
+
+def freq(review):
+    for i, v in enumerate(review):
+        if len(v) < 2:
+            review.pop(i)
+
+    count = Counter(review)
+    return count
+
+def below_threshold_len(max_len, nested_list):
+    cnt = 0
+    for s in nested_list:
+        if (len(s) <= max_len):
+            cnt = cnt + 1
+    print('전체 샘플 중 길이가 %s 이하인 샘플의 비율: %s' % (max_len, (cnt / len(nested_list)) * 100))
+
+
+stopwords = ['도', '는', '다', '의', '가', '이', '은', '한', '에', '하', '고', '을', '를', '인', '듯', '과', '와', '네', '들', '듯', '지',
+             '임', '게']
+
+total_data = pd.read_csv('./storageReview2.csv', sep=',')
+total_data['label'] = np.select([total_data.star > 3], [1], default=0)
+
+#total_data.drop_duplicates(subset=['review'], inplace=True)  # 중복제거 -> 변화없지만 그래도
+
+train_data, test_data = train_test_split(total_data, test_size=0.25, random_state=42)
+print('훈련용 리뷰의 개수 :', len(train_data))
+print('테스트용 리뷰의 개수 :', len(test_data))
+
+print(train_data.groupby('label').size().reset_index(name = 'count'))
+
+# 한글과 공백을 제외하고 모두 제거
+train_data['review'] = train_data['review'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]", "")
+train_data['review'].replace('', np.nan, inplace=True)
+print(train_data.isnull().sum())
+
+test_data.drop_duplicates(subset = ['review'], inplace=True) # 중복 제거
+test_data['review'] = test_data['review'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]", "")  # 정규 표현식 수행
+test_data['review'].replace('', np.nan, inplace=True)  # 공백은 Null 값으로 변경
+test_data = test_data.dropna(how='any') # Null 값 제거
+print('전처리 후 테스트용 샘플의 개수 :',len(test_data))
+
+
+
+
+
+train_data['tokenized'] = train_data['review'].apply(okt.morphs)
+train_data['tokenized'] = train_data['tokenized'].apply(lambda x: [item for item in x if item not in stopwords])
+
+test_data['tokenized'] = test_data['review'].apply(okt.morphs)
+test_data['tokenized'] = test_data['tokenized'].apply(lambda x: [item for item in x if item not in stopwords])
+
+print(train_data)
+
+negative_words = np.hstack(train_data[train_data.label == 0]['tokenized'].values)
+positive_words = np.hstack(train_data[train_data.label == 1]['tokenized'].values)
+
+negative_word_count = Counter(negative_words)
+print(negative_word_count.most_common(20))
+
+positive_word_count = Counter(positive_words)
+print(positive_word_count.most_common(20))
+
+#########################################################################################
+
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+text_len = train_data[train_data['label'] == 1]['tokenized'].map(lambda x: len(x))
+ax1.hist(text_len, color='red')
+ax1.set_title('Positive Reviews')
+ax1.set_xlabel('length of samples')
+ax1.set_ylabel('number of samples')
+print('긍정 리뷰의 평균 길이 :', np.mean(text_len))
+
+text_len = train_data[train_data['label'] == 0]['tokenized'].map(lambda x: len(x))
+ax2.hist(text_len, color='blue')
+ax2.set_title('Negative Reviews')
+fig.suptitle('Words in texts')
+ax2.set_xlabel('length of samples')
+ax2.set_ylabel('number of samples')
+print('부정 리뷰의 평균 길이 :', np.mean(text_len))
+plt.show()
+
+X_train = train_data['tokenized'].values
+y_train = train_data['label'].values
+X_test = test_data['tokenized'].values
+y_test = test_data['label'].values
+
+####################################여기부터 문제 쿠다텐서플로우#################################################
+
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(X_train)
+
+threshold = 2
+total_cnt = len(tokenizer.word_index)  # 단어의 수
+rare_cnt = 0  # 등장 빈도수가 threshold보다 작은 단어의 개수를 카운트
+total_freq = 0  # 훈련 데이터의 전체 단어 빈도수 총 합
+rare_freq = 0  # 등장 빈도수가 threshold보다 작은 단어의 등장 빈도수의 총 합
+
+# 단어와 빈도수의 쌍(pair)을 key와 value로 받는다.
+for key, value in tokenizer.word_counts.items():
+    total_freq = total_freq + value
+
+    # 단어의 등장 빈도수가 threshold보다 작으면
+    if (value < threshold):
+        rare_cnt = rare_cnt + 1
+        rare_freq = rare_freq + value
+
+print('단어 집합(vocabulary)의 크기 :', total_cnt)
+print('등장 빈도가 %s번 이하인 희귀 단어의 수: %s' % (threshold - 1, rare_cnt))
+print("단어 집합에서 희귀 단어의 비율:", (rare_cnt / total_cnt) * 100)
+print("전체 등장 빈도에서 희귀 단어 등장 빈도 비율:", (rare_freq / total_freq) * 100)
+
+############################## 여기까지 되는거 확인함 ##############################
+vocab_size = total_cnt - rare_cnt + 2
+print('단어 집합의 크기 :', vocab_size)
+
+tokenizer = Tokenizer(vocab_size, oov_token='OOV')
+tokenizer.fit_on_texts(X_train)
+X_train = tokenizer.texts_to_sequences(X_train)
+X_test = tokenizer.texts_to_sequences(X_test)
+
+print('리뷰의 최대 길이 :', max(len(l) for l in X_train))
+print('리뷰의 평균 길이 :', sum(map(len, X_train)) / len(X_train))
+plt.hist([len(s) for s in X_train], bins=50)
+plt.xlabel('length of samples')
+plt.ylabel('number of samples')
+plt.show()
+
+
+max_len = 100
+below_threshold_len(max_len, X_train)
+
+X_train = pad_sequences(X_train, maxlen=max_len)
+X_test = pad_sequences(X_test, maxlen=max_len)
+
+model = Sequential()
+model.add(Embedding(vocab_size, 100))
+model.add(GRU(128))
+model.add(Dense(1, activation='sigmoid'))
+
+es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=4)
+mc = ModelCheckpoint('best_model.h5', monitor='val_acc', mode='max', verbose=1, save_best_only=True)
+
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+history = model.fit(X_train, y_train, epochs=15, callbacks=[es, mc], batch_size=60, validation_split=0.2)
+
+loaded_model = load_model('best_model.h5')
+print("\n 테스트 정확도: %.4f" % (loaded_model.evaluate(X_test, y_test)[1]))
+
+def sentiment_predict(new_sentence):
+    new_sentence = okt.morphs(new_sentence)  # 토큰화
+    new_sentence = [word for word in new_sentence if not word in stopwords]  # 불용어 제거
+    encoded = tokenizer.texts_to_sequences([new_sentence])  # 정수 인코딩
+    pad_new = pad_sequences(encoded, maxlen=max_len)  # 패딩
+    score = float(loaded_model.predict(pad_new))  # 예측
+    if (score > 0.5):
+        print("{:.2f}% 확률로 긍정 리뷰입니다.".format(score * 100))
+    else:
+        print("{:.2f}% 확률로 부정 리뷰입니다.".format((1 - score) * 100))
+
+
+
+
+sentiment_predict('잘 쓰고 있어요. 설치 간단해서 좋네요.')
