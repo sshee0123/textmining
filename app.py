e7f9f3a9431cd96f878a1992896913d97fce2ee5
ji-yeon224 <shjh0107@gmail.com>
2021-06-11 01:36:10 +0900

    210611

--- app.py
+++ app.py
@@ -162,7 +162,7 @@ def recommend():
     #     result = dict(item_based[pdname].sort_values(ascending=False))
     # except KeyError as e:
     #     return None
-    return result
+    #return result
 
 ##################  user 데이터 없을 때 인기상품 1개 관련 상품 보여주기 ############################
 @app.route("/rec/nodata")
